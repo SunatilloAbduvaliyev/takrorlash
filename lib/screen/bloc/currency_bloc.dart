7fb2f85fd42082c31300dd7c06f7b78114a1aa6d
SunatilloAbduvaliyev <codepython632@gmail.com>
2024-04-30 12:05:50 +0500

    finish

---
+++ lib/screen/bloc/currency_bloc.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:takrorlash/data/local/local_storage.dart';
+import 'package:takrorlash/data/model/network_response_model/network_response_model.dart';
+import 'package:takrorlash/data/network/api_provider.dart';
+import 'package:takrorlash/screen/bloc/currency_event.dart';
+import 'package:takrorlash/screen/bloc/currency_state.dart';
+
+import '../../data/model/currency_model/currency_model.dart';
+import '../../services/services_locator.dart';
+
+class CurrencyBloc extends Bloc<CurrencyEvent, CurrencyState> {
+  CurrencyBloc() : super(CurrencyInitialState()) {
+    on<GetNetworkCurrencyEvent>(_getNetworkCurrency);
+    on<GetLocalCurrencyEvent>(_getLocalCurrency);
+  }
+
+  Future<void> _getNetworkCurrency(
+      GetNetworkCurrencyEvent event, Emitter<CurrencyState> emit) async {
+    emit(CurrencyLoadingState());
+    NetworkResponse response = await getIt.get<ApiProvider>().getCurrencies();
+    debugPrint('____________________________________response ${response.data}');
+    if (response.statusCode == 200) {
+      debugPrint('_________________________________________________${response.data.length}');
+      // List<CurrencyModel> localData =
+      //     await getIt.get<CurrencyController>().getAllCurrency();
+      // if (localData.isEmpty) {
+      //   for (int i = 0; i < response.data.length; i++) {
+      //     getIt.get<CurrencyController>().insertCurrency(response.data[i]);
+      //   }
+      // } else {
+      //   for (int i = 0; i < response.data.length - 1; i++) {
+      //     if (dataEqual(
+      //       dataOne: CurrencyModel.toJson(data: response.data[i]),
+      //       dataTwo: CurrencyModel.toJson(data: response.data[i + 1]),
+      //     )) {
+      //       getIt.get<CurrencyController>().insertCurrency(response.data[i]);
+      //     }
+      //   }
+      // }
+      emit(CurrencySuccessState(data: response.data));
+    } else if (response.statusCode == 404) {
+      List<CurrencyModel> localData =
+          await getIt.get<CurrencyController>().getAllCurrency();
+      if (localData.isEmpty) {
+        emit(CurrencyNetworkState(isLocal: true));
+      } else {
+        emit(CurrencySuccessState(data: localData));
+      }
+    }else{
+      throw Exception(response.errorText);
+    }
+  }
+
+  Future<void> _getLocalCurrency(
+      GetLocalCurrencyEvent event, Emitter<CurrencyState> emit) async {
+    emit(CurrencySuccessState(
+      data: await getIt.get<CurrencyController>().getAllCurrency(),
+    ));
+  }
+}
